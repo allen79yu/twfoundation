768a401cb9acce2ad657cfca9b86485351ba509d
allen79yu <allen79yu@gmail.com>
2014-07-26 22:34:22 +0800

    read file

--- read_file.php
+++ read_file.php
@@ -3,40 +3,88 @@
 	include("lib/LIB_parse.php");
 	$array[]=1;
 	$tmp[]=1;
-	$file=fopen("org_data_utf8.txt","r");
-	$csv=fopen("data_test.csv","a+");//file setting
-	while(! feof($file)){
-	array_push($array,fgets($file));
-	}
-	$response_start=http_get ("http://cdcb.judicial.gov.tw/abbs/wkw/WHD6K03.jsp?ab=AAAdkxAAHAAACxwAAF&bc=033&cd=%AA%6B%B5%6E++++++++++++++++&de=000001&ef=TPD&fg=1","http://cdcb.judicial.gov.tw/abbs/wkw/");
+	$court_name = array(
+  'TPD',
+  'PCD',
+  'SLD',
+  'TYD',
+  'SCD',
+  'MLD',
+  'TCD',
+  'NTD',
+  'CHD',
+  'ULD',
+  'CYD',
+  'TND',
+  'KSD',
+  'PTD',
+  'TTD',
+  'HLD',
+  'ILD',
+  'KLD',
+  'PHD',
+  'KSY',
+  'LCD',
+  'KMD',
+);
+for($index=0;$index<count($court_name);$index++){	
+	$row=array();
+	$court=$court_name[$index];
+	$filename="output/".$court.".csv";
+	$output="output/".$court."_detail.csv";
+
+if(file_exists($filename)){
+    $rows = csv2array($filename);
+	$first_run = FALSE;
+  }
+  else{
+	$first_run = TRUE;
+  }
+  $meta_flag=FALSE;
+ for($x=1;$x<11;$x++){ 
+	
+	$response_start=http_get ( str_replace("WHD6K05","WHD6K03",$rows[$x][2]),"http://cdcb.judicial.gov.tw/abbs/wkw/");
 	$response_parse_start = parse_array($response_start["FILE"],"<td","</td>");
-		for($i=1;$i<count($response_parse_start);$i++){
+	if(!$meta_flag){
+	for($i=1;$i<count($response_parse_start);$i++){
 		if($i%2==1){
-			echo "meta";
-			echo trim(preg_replace('/[\n\r\t]/','',strip_tags($response_parse_start[$i]))).",";
-			fwrite($csv,trim(preg_replace('/[\n\r\t]/','',strip_tags($response_parse_start[$i]))).",");
-		}
+			echo iconv("BIG5","UTF-8", trim(preg_replace('/[\n\r\t]/','',strip_tags($response_parse_start[$i])))).",";
+			 $row =  iconv("BIG5","UTF-8", trim(preg_replace('/[\n\r\t]/','',strip_tags($response_parse_start[$i])))).",";
+			 file_put_contents($output, $row, FILE_APPEND);
+			$meta_flag=TRUE;
+		}	
 	}
-	fwrite($csv,"\r\n");
-	for($x=1;$x<11;$x++){
-		$tmp = explode('|',$array[$x],2);
-		echo "1".$tmp[1]."</br>";
-	$response=http_get ($tmp[1],"http://cdcb.judicial.gov.tw/abbs/wkw/");
-	echo $response["FILE"];
-	//get response
-	//parse
-	$response_parse = parse_array($response["FILE"],"<td","</td>");	
-	for($i=2;$i<count($response_parse);$i++){
+		echo "</br>";
+		 file_put_contents($output, "\r\n", FILE_APPEND);
+	}
+	for($i=1;$i<count($response_parse_start);$i++){
 		if($i%2==0){
-			echo trim(preg_replace('/[\n\r\t]/','',strip_tags($response_parse[$i]))).",";
-			fwrite($csv,trim(preg_replace('/[\n\r\t]/','',strip_tags($response_parse[$i]))).",");
-		}
-		if($i==59){
-			echo trim(preg_replace('/[\n\r\t]/','',strip_tags($response_parse[$i])))."</br>";
-			fwrite($csv,trim(preg_replace('/[\n\r\t]/','',strip_tags($response_parse[$i])))."\r\n");
-			}
-		}
+			$row =  iconv("BIG5","UTF-8", trim(preg_replace('/[\n\r\t]/','',strip_tags($response_parse_start[$i])))).",";
+			 file_put_contents($output, $row, FILE_APPEND);
+			echo iconv("BIG5","UTF-8", trim(preg_replace('/[\n\r\t]/','',strip_tags($response_parse_start[$i])))).",";
+		}	
 	}
-fclose($file);
-fclose($csv);
+		echo "</br>";
+		file_put_contents($output, "\r\n", FILE_APPEND);
+}
+}
+function csv2array($filename){
+  $data = array();
+//  copy($filename, $filename.'.bak');
+  if (($handle = fopen($filename, "r")) !== FALSE) {
+    while (($col = fgetcsv($handle, 0, ",")) !== FALSE) {
+      $data[$col[0]] = $col;
+    }
+    fclose($handle);
+  }
+  krsort($data);
+  return $data;
+}
+function array2csv($array){
+  $csv = '';
+  foreach($array as $k => $v){
+    $csv .= '"'.implode('","', $v).'"'."\n";
+  }
+  return $csv;
+}
 ?>
